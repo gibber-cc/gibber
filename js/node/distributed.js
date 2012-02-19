ffeb93f8c8109d8d017e220a4c5b6606b0bcac5a
thecharlie <charlie@charlie-roberts.com>
2012-02-18 17:22:09 -0800

    messages from slaves only sent to master

--- js/node/distributed.js
+++ js/node/distributed.js
@@ -1,9 +1,31 @@
 var io = require('socket.io').listen(8080);
 
+var master = null;
+
 io.sockets.on('connection', function (socket) {
+	socket.addr = socket.handshake.address.address;
+	socket.port = socket.handshake.address.port;
+	socket.isMaster = false;
+	
 	socket.on('message', function (msg) {
 		console.log("MESSAGE " + msg);
-		socket.broadcast.send(msg);
+		if(master !== null) {
+			master.send(msg);
+		}
 	});
-	socket.on('disconnect', function () { });
+	
+	socket.on('master', function(msg) {
+		if(master === null) {
+			console.log("SETTING MASTER SOCKET");
+			master = socket;
+		}
+	});
+	
+	socket.on('disconnect', function () { 
+		console.log("DISCONNECT : " + this.addr);
+		if(master === this) {
+			console.log("DISCONNECTING MASTER");
+			master = null;
+		}
+	});
 });
