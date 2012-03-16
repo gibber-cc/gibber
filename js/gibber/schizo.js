fe1e2fdc387539becd87f5c01e4b2a2cdcf33fd1
thecharlie <charlie@charlie-roberts.com>
2012-03-16 02:28:33 -0700

    fixed bugs in schizo

--- js/gibber/schizo.js
+++ js/gibber/schizo.js
@@ -3,7 +3,8 @@ function Schizo(chance, rate, length, shouldRandomizePitch, shouldRandomizeRever
 		chance: (typeof chance !== "undefined") ? chance : .5,		
 		rate: (typeof rate !== "undefined") ? rate : _1,
 		length: (typeof length !== "undefined") ? length : _4,
-		shouldRandomizeReverse : (typeof shouldRandomizeReverse === "undefined") ? true : false,
+		shouldRandomizeReverse : (typeof shouldRandomizeReverse === "undefined") ? true : shouldRandomizeReverse,
+		shouldRandomizePitch : (typeof shouldRandomizePitch === "undefined") ? true : shouldRandomizePitch,		
 		name : "Schizo",
 		type: "fx",
 		gens :  [],
@@ -11,26 +12,31 @@ function Schizo(chance, rate, length, shouldRandomizePitch, shouldRandomizeRever
 		value : 0,
 		readIndex : 0,
 		writeIndex : 0,
+		increment : 1,
 		phase : 0,
 		isCrazy : false,
 		crazyTime : 0,
 		reverse : false,
+		pitchShifting : false,
 		mix : 1,
 
 		pushSample : function(sample) {
 			var val = sample;
 			
+			if(isNaN(sample)) G.log( "ALERT");
 			this.writeBuffer[this.writeIndex++] = val;
 			if (this.writeIndex >= this.writeBufferLength) {
 				this.writeIndex = 0;
 			}
-			if(++this.phase === this.rate) {
+			if(++this.phase === Math.floor(this.rate)) {
 				if(!this.isCrazy) {
 					if(Math.random() < this.chance) {
+						G.log("CRAZY");
+						
 						this.isCrazy = true;
 						var readHead = this.writeIndex - (this.length * rndi(1,4));
-						if(readHead < 0) {
-							readHead += this.readBufferLength; // loop through end
+						while(readHead < 0) {
+							readHead += this.length; // loop through end
 						}
 						readHead = Math.floor(readHead);
 						for(var i = 0; i < this.length; i++) {
@@ -43,25 +49,33 @@ function Schizo(chance, rate, length, shouldRandomizePitch, shouldRandomizeRever
 						if(this.shouldRandomizeReverse) {
 							this.reverse = (Math.random() > .5) ? true : false;
 						}
-						if(this.reverse) {
-							this.readIndex = this.length - 1;
-						}else{
-							this.readIndex = 0;
+						this.readIndex = (this.reverse) ? this.length - 1 : 0;
+
+						if(this.shouldRandomizePitch) {
+							this.pitchShifting = (Math.random() > .5) ? true : false;
 						}
+						this.increment = (this.pitchShifting) ? rndf(.5, 2) : 1;
+						
+						this.readIndex = Math.floor(this.readIndex);
 						this.crazyTime = 0;
 					}
 				}
 				this.phase = 0;
 			}else{
 				if(this.isCrazy) {
+					val = Sink.interpolate(this.readBuffer, this.readIndex);
+					if(G.debug) G.log(this.increment + " : " + this.readIndex + " : " + val);
+					
 					if(this.reverse) {
-						val = this.readBuffer[this.readIndex--];
-						if(this.readIndex < 0) {
+						this.readIndex -= this.increment;
+						
+						if(this.readIndex < 0 || this.readIndex >= this.length) {
 							this.readIndex = this.length - 1;
 						}
 					}else{
-						val = this.readBuffer[this.readIndex++];
-						if(this.readIndex >= this.length) {
+						this.readIndex += this.increment
+						
+						if(this.readIndex >= this.length || this.readIndex < 0) {
 							this.readIndex = 0;
 						}
 					}
@@ -69,6 +83,7 @@ function Schizo(chance, rate, length, shouldRandomizePitch, shouldRandomizeRever
 					if(this.crazyTime++ > this.rate) {
 						this.isCrazy = false;
 						this.crazyTime = 0;
+						this.increment = 0;
 						G.log("ENDING CRAZY TIME");
 					}
 				}
@@ -83,10 +98,9 @@ function Schizo(chance, rate, length, shouldRandomizePitch, shouldRandomizeRever
 	};
 	
 	that.writeBuffer = new Float32Array(Gibber.sampleRate * 2);
+	that.writeBufferLength = Gibber.sampleRate * 2;		
 	that.readBuffer  = new Float32Array(that.length);
-	that.readBufferLength = that.length;
-	that.writeBufferLength = Gibber.sampleRate * 2;	
-	
+
 	Gibber.addModsAndFX.call(that);
 	
 	// TODO: Fix so this changes the speed of the LFO
