a547bd59a3b7bbdf973e36e04bed8a72f796a05b
thecharlie <charlie@charlie-roberts.com>
2012-08-23 17:27:30 -0700

    stereo comb and allpass filters moving towards reverb

--- js/gibberish/lib/effects.js
+++ js/gibberish/lib/effects.js
@@ -1,252 +1,7 @@
 define([], function() {
     return {
 		init: function(gibberish) {
-			gibberish.BufferShuffler = Gen({
-				name:"BufferShuffler",
-				acceptsInput: true,
-				props: { chance:.25, rate:11025, length:22050, reverseChange:.5, pitchChance:.5, pitchMin:.25, pitchMax:2, channels:2 },
-				upvalues: {
-					buffer : null,
-					readIndex : 0,
-					writeIndex : 0,
-					randomizeCheckIndex : 0,
-					shuffleTimeKeeper : 0,
-					isShuffling : 0,
-					random : Math.random,
-					bufferLength : 88200,
-					fadeIndex : 0,
-					fadeAmount : 1,
-					isFadingWetIn : false,
-					isFadingDryIn : false,
-					reversed : false,
-					interpolate : Gibberish.interpolate,
-					pitchShifting : false,
-					speed : 1,
-					isBufferFull : false,
-				},
-				
-				init: function() {
-					if(this.channels === 1) {
-						this.function.setBuffer(new Float32Array(88200));
-					}else{
-						console.log("INIT DOUBLE BUFFER");
-						this.function.setBuffer( [new Float32Array(88200), new Float32Array(88200)] );
-						console.log(this.function.getBuffer());
-					}
-				},
-				
-				callback : function(sample, chance, rate, length, reverseChance, pitchChance, pitchMin, pitchMax, channels) {
-					var isStereo = typeof sample[0] === "number";
-					if(!isShuffling) {
-						if(isStereo) {
-							//console.log(buffer);
-							buffer[0][writeIndex] = sample[0];
-							buffer[1][writeIndex++] = sample[1]
-							//console.log("BLAH");
-						}else{
-							buffer[writeIndex++] = sample;
-						}	
-						writeIndex %= bufferLength;
-					
-						isBufferFull = writeIndex === 0 ? 1 : isBufferFull; // don't output buffered audio until a buffer is full... otherwise you just get a gap
-						randomizeCheckIndex += !isShuffling;
-					
-						if(randomizeCheckIndex % rate == 0 && random() < chance) {
-							reversed = random() < reverseChance;
-							isShuffling = true;
-							if(!reversed) {
-								readIndex = writeIndex - length;
-								if(readIndex < 0) readIndex = bufferLength + readIndex;
-							}
-							pitchShifting = random() < pitchChance;
-							if(pitchShifting) {
-								speed = window.rndf(pitchMin, pitchMax);
-							}
-							fadeAmount = 1;
-							isFadingWetIn = true;
-							isFadingDryIn = false;
-						}
-					}else if(++shuffleTimeKeeper % (length - 400) === 0) {
-						isFadingWetIn = false;
-						isFadingDryIn = true;
-						fadeAmount = 1;
-						shuffleTimeKeeper = 0;
-					}
-				
-					var out, index;
-					readIndex += reversed ? speed * -1 : speed;
-					if(readIndex < 0) {
-						readIndex += bufferLength;
-					}else if( readIndex > bufferLength) {
-						readIndex -= bufferLength;
-					}
-					
-					var outSample;
-					if(isStereo) {
-						outSample = [
-							interpolate(buffer[0], readIndex),
-							interpolate(buffer[1], readIndex),
-						];
-					}else{
-						outSample = interpolate(buffer, readIndex);
-					}
-				
-					if(isFadingWetIn) {						
-						fadeAmount -= .0025;
-						if(isStereo){
-							out = [];
-							out[0] = (outSample[0] * (1 - fadeAmount)) + (sample[0] * fadeAmount);
-							out[1] = (outSample[1] * (1 - fadeAmount)) + (sample[1] * fadeAmount);							
-						}else{
-							out = (outSample * (1 - fadeAmount)) + (sample * fadeAmount);
-						}
-						if(fadeAmount <= .0025) isFadingWetIn = false;
-					}else if(isFadingDryIn) {						
-						fadeAmount -= .0025;
-						if(isStereo){
-							out = [];
-							out[0] = (outSample[0] * (fadeAmount)) + (sample[0] * (1 - fadeAmount));
-							out[1] = (outSample[1] * (fadeAmount)) + (sample[1] * (1 - fadeAmount));
-						}else{
-							out = (outSample * (fadeAmount)) + (sample * (1 - fadeAmount));
-						}
-						if(fadeAmount <= .0025) { 
-							isFadingDryIn = false;
-							isShuffling = false;
-							reversed = false;
-							speed = 1;
-							pitchShifting = 0;
-						}
-					}else{
-						if(isStereo) {
-							out = [
-								isShuffling && isBufferFull ? outSample[0] : sample[0],
-								isShuffling && isBufferFull ? outSample[1] : sample[1],
-							];
-						}else{
-							out = isShuffling && isBufferFull ? outSample : sample;
-						}
-					}
-					return out;
-				},
-			});
-				
-			/*}
-			BufferShuffler : function(properties) {
-				var that = {
-					type:		"BufferShuffler",
-					category:	"FX",
-					chance: 	.25,		
-					rate: 		11025,
-					length:		22050,
-					reverseChance : .5,
-					pitchChance : .5,
-					pitchMin : .25,
-					pitchMax : 2,
-					mix : 1,
-				};
-			
-				Gibberish.extend(that, new Gibberish.ugen(that));
-				if(typeof properties !== "undefined") {
-					Gibberish.extend(that, properties);
-				}
-
-				that.buffer = new Float32Array(that.length * 2);
-
-				that.symbol = Gibberish.generateSymbol(that.type);
-				Gibberish.masterInit.push(that.symbol + " = Gibberish.make[\"BufferShuffler\"]();");
-				window[that.symbol] = Gibberish.make["BufferShuffler"](that.buffer);
-				that._function = window[that.symbol];
-
-				//Gibberish.defineProperties( that, ["time", "feedback"] );
-
-				return that;
-			},
-
-			makeBufferShuffler : function(buffer) {
-				var readIndex = 0;
-				var writeIndex = 0;
-				var randomizeCheckIndex = 0;
-				var shuffleTimeKeeper = 0;
-				var isShuffling = 0;
-				var random = Math.random;
-				var bufferLength = buffer.length;
-				var fadeIndex = 0;
-				var fadeAmount = 1;
-				var isFadingWetIn = false;
-				var isFadingDryIn = false;
-				var reversed = false;
-				var interpolate = Gibberish.interpolate;
-				var pitchShifting = false;
-				var speed = 1;
-				var init = 0;
-
-			
-				var output = function(sample, chance, rate, length, reverseChance, pitchChance, pitchMin, pitchMax) {
-					//if(writeIndex % 5000 === 0) console.log(chance, rate, length, randomizeCheckIndex);
-					if(!isShuffling) {
-						buffer[writeIndex++] = sample;
-						writeIndex %= buffer.length;
-					
-						init = writeIndex === 0 ? 1 : init; // don't output buffered audio until a buffer is full... otherwise you just get a gap
-					
-						randomizeCheckIndex += !isShuffling;
-					
-						if(randomizeCheckIndex % rate == 0 && random() < chance) {
-							reversed = random() < reverseChance;
-							isShuffling = true;
-							if(!reversed) {
-								readIndex = writeIndex - length;
-								if(readIndex < 0) readIndex = bufferLength + readIndex;
-							}
-							pitchShifting = random() < pitchChance;
-							if(pitchShifting) {
-								speed = window.rndf(pitchMin, pitchMax);
-							}
-							fadeAmount = 1;
-							isFadingWetIn = true;
-							isFadingDryIn = false;
-						}
-					}else if(++shuffleTimeKeeper % (length - 400) === 0) {
-						isFadingWetIn = false;
-						isFadingDryIn = true;
-						fadeAmount = 1;
-						shuffleTimeKeeper = 0;
-					}
-				
-					var out, index;
-					readIndex += reversed ? speed * -1 : speed;
-					if(readIndex < 0) {
-						readIndex += bufferLength;
-					}else if( readIndex > bufferLength) {
-						readIndex -= bufferLength;
-					}
-					var outSample = interpolate(buffer, readIndex);
-				
-					if(isFadingWetIn) {
-						fadeAmount -= .0025;
-						out = (outSample * (1 - fadeAmount)) + (sample * fadeAmount);
-						if(fadeAmount <= .0025) isFadingWetIn = false;
-					}else if(isFadingDryIn) {
-						fadeAmount -= .0025;
-						out = (outSample * (fadeAmount)) + (sample * (1 - fadeAmount));
-						if(fadeAmount <= .0025) { 
-							isFadingDryIn = false;
-							isShuffling = false;
-							reversed = false;
-							speed = 1;
-							pitchShifting = 0;
-						}
-					}else{
-						out = isShuffling && init ? outSample : sample;
-					}
-
-					return out;
-				};
-
-				return output;
-			},
-			*/	
+	
 			gibberish.SoftClip = Gen({
 				name:"SoftClip",
 				acceptsInput:true,	
@@ -488,141 +243,224 @@ define([], function() {
 					return sample;
 				},	
 			});
+			
+			gibberish.BufferShuffler = Gen({
+				name:"BufferShuffler",
+				acceptsInput: true,
+				props: { chance:.25, rate:11025, length:22050, reverseChange:.5, pitchChance:.5, pitchMin:.25, pitchMax:2, channels:2 },
+				upvalues: {
+					buffer : null,
+					readIndex : 0,
+					writeIndex : 0,
+					randomizeCheckIndex : 0,
+					shuffleTimeKeeper : 0,
+					isShuffling : 0,
+					random : Math.random,
+					bufferLength : 88200,
+					fadeIndex : 0,
+					fadeAmount : 1,
+					isFadingWetIn : false,
+					isFadingDryIn : false,
+					reversed : false,
+					interpolate : Gibberish.interpolate,
+					pitchShifting : false,
+					speed : 1,
+					isBufferFull : false,
+				},
+				
+				init: function() {
+					if(this.channels === 1) {
+						this.function.setBuffer(new Float32Array(88200));
+					}else{
+						this.function.setBuffer( [new Float32Array(88200), new Float32Array(88200)] );
+					}
+				},
+				
+				callback : function(sample, chance, rate, length, reverseChance, pitchChance, pitchMin, pitchMax, channels) {
+					var isStereo = typeof sample[0] === "number";
+					if(!isShuffling) {
+						if(isStereo) {
+							//console.log(buffer);
+							buffer[0][writeIndex] = sample[0];
+							buffer[1][writeIndex++] = sample[1]
+							//console.log("BLAH");
+						}else{
+							buffer[writeIndex++] = sample;
+						}	
+						writeIndex %= bufferLength;
+					
+						isBufferFull = writeIndex === 0 ? 1 : isBufferFull; // don't output buffered audio until a buffer is full... otherwise you just get a gap
+						randomizeCheckIndex += !isShuffling;
+					
+						if(randomizeCheckIndex % rate == 0 && random() < chance) {
+							reversed = random() < reverseChance;
+							isShuffling = true;
+							if(!reversed) {
+								readIndex = writeIndex - length;
+								if(readIndex < 0) readIndex = bufferLength + readIndex;
+							}
+							pitchShifting = random() < pitchChance;
+							if(pitchShifting) {
+								speed = window.rndf(pitchMin, pitchMax);
+							}
+							fadeAmount = 1;
+							isFadingWetIn = true;
+							isFadingDryIn = false;
+						}
+					}else if(++shuffleTimeKeeper % (length - 400) === 0) {
+						isFadingWetIn = false;
+						isFadingDryIn = true;
+						fadeAmount = 1;
+						shuffleTimeKeeper = 0;
+					}
+				
+					var out, index;
+					readIndex += reversed ? speed * -1 : speed;
+					if(readIndex < 0) {
+						readIndex += bufferLength;
+					}else if( readIndex > bufferLength) {
+						readIndex -= bufferLength;
+					}
+					
+					var outSample;
+					if(isStereo) {
+						outSample = [
+							interpolate(buffer[0], readIndex),
+							interpolate(buffer[1], readIndex),
+						];
+					}else{
+						outSample = interpolate(buffer, readIndex);
+					}
+				
+					if(isFadingWetIn) {						
+						fadeAmount -= .0025;
+						if(isStereo){
+							out = [];
+							out[0] = (outSample[0] * (1 - fadeAmount)) + (sample[0] * fadeAmount);
+							out[1] = (outSample[1] * (1 - fadeAmount)) + (sample[1] * fadeAmount);							
+						}else{
+							out = (outSample * (1 - fadeAmount)) + (sample * fadeAmount);
+						}
+						if(fadeAmount <= .0025) isFadingWetIn = false;
+					}else if(isFadingDryIn) {						
+						fadeAmount -= .0025;
+						if(isStereo){
+							out = [];
+							out[0] = (outSample[0] * (fadeAmount)) + (sample[0] * (1 - fadeAmount));
+							out[1] = (outSample[1] * (fadeAmount)) + (sample[1] * (1 - fadeAmount));
+						}else{
+							out = (outSample * (fadeAmount)) + (sample * (1 - fadeAmount));
+						}
+						if(fadeAmount <= .0025) { 
+							isFadingDryIn = false;
+							isShuffling = false;
+							reversed = false;
+							speed = 1;
+							pitchShifting = 0;
+						}
+					}else{
+						if(isStereo) {
+							out = [
+								isShuffling && isBufferFull ? outSample[0] : sample[0],
+								isShuffling && isBufferFull ? outSample[1] : sample[1],
+							];
+						}else{
+							out = isShuffling && isBufferFull ? outSample : sample;
+						}
+					}
+					return out;
+				},
+			});
+			
+			// adapted from audioLib.js, in turn adapted from Freeverb source code
+			// https://ccrma.stanford.edu/~jos/pasp/Lowpass_Feedback_Comb_Filter.html)
+			gibberish.LowPassComb = Gen({
+				name:"LowPassComb",
+				acceptsInput: true,
+				props: { feedback: .84, time: 1200, damping:.2, channels:2 },
+				upvalues:{
+					buffer:null,
+					index : 0,
+					store : 0,
+					store_ : 0,
+				},
+				init: function() {
+					if(this.channels === 1) {
+						this.function.setBuffer(new Float32Array(this.time));
+					}else{
+						this.function.setBuffer( [new Float32Array(this.time), new Float32Array(this.time)] );
+					}
+				},
+				callback: function(sample, feedback, time, damping) {
+					var currentPos = ++index % time;
+					var out;
+					if(typeof sample[0] === "number") {
+						out = [
+							buffer[0][currentPos],
+							buffer[1][currentPos],
+						];
+						store = (out[0] * .8) + (store * .2);
+						buffer[0][currentPos] = sample[0] + (store * feedback);
+						
+						store_ = (out[1] * .8) + (store_ * .2);
+						buffer[1][currentPos] = sample[1] + (store_ * feedback);
+					}else{
+						
+						out = buffer[currentPos];
+						
+						store = (out * .8) + (store * .2);
+						buffer[currentPos] = sample + (store * feedback);
+					}					
+					return out;
+				},
+			});
+			gibberish.AllPass = Gen({
+				name:"AllPass",
+				acceptsInput: true,
+				props: { feedback:.5, time:500, channels:2 },
+				upvalues: { buffer: null, index:-1, bufferLength:500 },
+				
+				init: function() {
+					if(this.channels === 1) {
+						this.function.setBuffer(new Float32Array(this.time));
+					}else{
+						this.function.setBuffer( [new Float32Array(this.time), new Float32Array(this.time)] );
+					}
+				},
+				
+				callback: function(sample, feedback) {
+					index = ++index % bufferLength;
+					var out;
+					
+					if(typeof sample[0] === "number") {
+						var bufferSample1 = buffer[0][index];
+						var bufferSample2 = buffer[1][index];
+						
+						out = [
+							-1 * sample[0] + bufferSample1,
+							-1 * sample[1] + bufferSample2,
+						];
+						
+						buffer[0][index] = sample[0] + (bufferSample1 * feedback);
+						buffer[1][index] = sample[1] + (bufferSample2 * feedback);						
+					}else{
+						var bufferSample = buffer[index];
 
+						out = -sample + bufferSample;
+						buffer[index] = sample + (bufferSample * feedback);
+					}
+					
+					return out;
+				},
+			});
+			
 			gibberish.generators.Reverb = gibberish.createGenerator(["source", "roomSize", "damping", "wet", "dry" ], "{0}( {1},{2},{3},{4},{5} )");
 			gibberish.make["Reverb"] = this.makeReverb;
 			gibberish.Reverb = this.Reverb;
-
-			gibberish.generators.AllPass = gibberish.createGenerator(["source", "time", "feedback"], "{0}( {1}, {2}, {3} )");
-			gibberish.make["AllPass"] = this.makeAllPass;
-			gibberish.AllPass = this.AllPass;
-
-			gibberish.generators.Comb = gibberish.createGenerator(["source", "time", "feedback"], "{0}( {1}, {2}, {3} )");
-			gibberish.make["Comb"] = this.makeComb;
-			gibberish.Comb = this.Comb;
-			
-			/*
-			gibberish.generators.BufferShuffler = gibberish.createGenerator(["source","chance", "rate", "length", "reverseChance", "pitchChance", "pitchMin", "pitchMax"], "{0}( {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8} )");
-			gibberish.make["BufferShuffler"] = this.makeBufferShuffler;
-			gibberish.BufferShuffler = this.BufferShuffler;
-			*/
 			
 			gibberish.generators.Bus = gibberish.createGenerator(["senders", "amp"], "{0}( {1}, {2} )");
 			gibberish.make["Bus"] = this.makeBus;
 			gibberish.Bus = this.Bus;
-		},
-
-		AllPass : function(time, feedback) {
-			var that = {
-				type:		"AllPass",
-				category:	"FX",
-				feedback:	feedback || .5,
-				time:		time || 500,
-				buffer:		new Float32Array(time || 500),
-				source:		null,
-			};
-			Gibberish.extend(that, new Gibberish.ugen(that) );
-
-			that.symbol = Gibberish.generateSymbol(that.type);
-			Gibberish.masterInit.push(that.symbol + " = Gibberish.make[\"AllPass\"]();");
-			window[that.symbol] = Gibberish.make["AllPass"](that.buffer, that.time, that.feedback);
-			that._function = window[that.symbol];
-
-			Gibberish.defineProperties( that, ["feedback"] );
-
-			// todo: this doesn't seem to be working... the buffer might need to be resampled.
-			(function(obj) {
-				var _time = obj.time;
-			    Object.defineProperty(that, "time", {
-					get: function() { return _time; },
-					set: function(value) {
-						if(_time !== value) {
-							_time = value;
-							obj.buffer = new Float32Array(value);
-							Gibberish.dirty(that);
-						}
-					},
-				});
-			})(that);
-
-			return that;
-		},
-
-		makeAllPass : function(buffer, feedback) {
-			//console.log("ALL PASS", _buffer.length, _feedback);
-			var bufferLength = buffer.length;
-			var index = -1;
-
-			var output = function(inputSample) {
-				index = ++index % bufferLength;
-				var bufferSample = buffer[index];
-
-				var out = -inputSample + bufferSample;
-				buffer[index] = inputSample + (bufferSample * feedback);
-
-				return out;
-			};
-
-			return output;
-		},
-		// adapted from audioLib.js, in turn adapted from Freeverb source code
-		// NOTE : this is actually a lowpass-feedback-comb filter (https://ccrma.stanford.edu/~jos/pasp/Lowpass_Feedback_Comb_Filter.html)
-		// TODO : rename accordingly?
-		Comb : function(time, feedback, damping) {
-			var that = {
-				type:		"Comb",
-				category:	"FX",
-				feedback:	feedback || .84,
-				time:		time || 1200,
-				buffer:		new Float32Array(time || 1200),
-				damping:	damping || .2,
-				source:		null,
-			};
-			Gibberish.extend(that, new Gibberish.ugen(that));
-
-			that.symbol = Gibberish.generateSymbol(that.type);
-			Gibberish.masterInit.push(that.symbol + " = Gibberish.make[\"Comb\"]();");
-			window[that.symbol] = Gibberish.make["Comb"](that.buffer, that.feedback, that.damping);
-			that._function = window[that.symbol];
-
-			Gibberish.defineProperties( that, ["feedback"] );
-
-			// todo: this doesn't seem to be working... the buffer might need to be resampled.
-			// (function(obj) {
-			// 	var _time = obj.time;
-			//     Object.defineProperty(that, "time", {
-			// 		get: function() { return _time; },
-			// 		set: function(value) {
-			// 			if(_time !== value) {
-			// 				_time = value;
-			// 				obj.buffer = new Float32Array(value);
-			// 				that.dirty = true;
-			// 				Gibberish.dirty = true;
-			// 			}
-			// 		},
-			// 	});
-			// })(that);
-
-			return that;
-		},
-
-		makeComb : function(buffer, feedback, damping) {
-			//console.log("COMB CHECK", _feedback, _damping, _buffer.length);
-			var invDamping = 1 - damping;
-			var time = buffer.length;
-			var index = 0;
-			var store = 0;
-
-			var output = function(inputSample) {
-				var currentPos = ++index % time;
-				var sample = buffer[currentPos];
-				store = (sample * .8) + (store * .2);
-				buffer[currentPos] = inputSample + (store * feedback);
-
-				return sample;
-			};
-
-			return output;
 		},
 
 		// adapted from audioLib.js
