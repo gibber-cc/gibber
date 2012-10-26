b87d3cdfaf22979237dff4387a5bb9002c36c004
thecharlie <charlie@charlie-roberts.com>
2012-10-26 14:22:56 -0700

    fix for republicish 2

--- js/gibber/environment.js
+++ js/gibber/environment.js
@@ -146,11 +146,14 @@ define([
 				
 				var b2 = $("<button>claim code</button>");
 				b2.codeblockNumber = msg.codeblockNumber;
-				$(b2).on('mouseup', function() {
-					//console.log("CLAIMING CODE????", b2.codeblockNumber, G.E.R.name);
-					G.E.socket.emit('claimCode', {name:G.E.R.name, codeNumber:b2.codeblockNumber});
-				});
-				$(b2).attr("id", "block"+b2.codeblockNumber);
+				(function() {
+					var num = msg.codeblockNumber;
+					$(b2).on('mouseup', function() {
+						//console.log("CLAIMING CODE????", b2.codeblockNumber, G.E.R.name);
+						G.E.socket.emit('claimCode', {name:G.E.R.name, codeNumber:num});
+					});
+					$(b2).attr("id", "block"+num);
+				})();
 				
 				$(d).append(b2);
 				
