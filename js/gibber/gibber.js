2c7311e6ed961a07c8c9a8550e2276c661baa5d3
charlieroberts <charlie@charlie-roberts.com>
2014-03-19 14:22:21 -0700

    moved pubsub to main.js

--- js/gibber/gibber.js
+++ js/gibber/gibber.js
@@ -26,17 +26,7 @@ window.Gibber = window.G = {
       'gibber/utilities',
       'external/esprima',
 			], 'gibber', function() {
-      
-      // pubsub as taken from here: https://gist.github.com/addyosmani/1321768
-      (function( $ ) {
-        var o = $( {} );
-
-        $.subscribe = function() { o.on.apply( o, arguments ) }
-        $.unsubscribe = function() { o.off.apply( o, arguments ) }
-        $.publish = function() { o.trigger.apply( o, arguments ) }
-
-      }( jQuery ))
-        
+  
       Gibber.Audio = Gibberish
       
       $.extend( window, Gibber.Busses )       
--- js/main.js
+++ js/main.js
@@ -2,6 +2,16 @@
 $script( ['external/jquery.min', 'external/gibberish.2.0.min'], 'GO')
 
 $script.ready( 'GO', function() {
+  // pubsub as taken from here: https://gist.github.com/addyosmani/1321768
+  (function( $ ) {
+    var o = $( {} );
+
+    $.subscribe = function() { o.on.apply( o, arguments ) }
+    $.unsubscribe = function() { o.off.apply( o, arguments ) }
+    $.publish = function() { o.trigger.apply( o, arguments ) }
+
+  }( jQuery ))
+  
   $script( 'gibber/gibber', function() {
     $script( 'gibber/environment', 'environment', function() {
     
