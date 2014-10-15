9363948431fa02803c20a706cbb13360d66a3b01
charlieroberts <charlie@charlie-roberts.com>
2014-10-14 17:52:05 -0700

    seq now accepts array of functions as first argument (optionally)

--- js/index.js
+++ js/index.js
@@ -23659,7 +23659,7 @@ module.exports.outputCurves= {
   var Seq = function() {
     var obj = {}, seq, hasScale, keyList = []
     
-    if( typeof arguments[0]  === 'object' ) {
+    if( typeof arguments[0]  === 'object' && ! Array.isArray( arguments[0] ) ) {
       var arg = arguments[0],
           durationsType = typeof arg.durations,
           targetsType = typeof arg.target ,
@@ -23736,13 +23736,13 @@ module.exports.outputCurves= {
           }]
         }
       }  
-    }else if( typeof arguments[0] === 'function' ){
+    }else if( typeof arguments[0] === 'function' || Array.isArray( arguments[0] ) ){
       obj.seqs = [{
         key:'functions',
-        values: [ arguments[ 0 ] ],
+        values: Array.isArray( arguments[0] ) ? arguments[0] : [ arguments[ 0 ] ],
         durations: Gibber.Clock.time( arguments[ 1 ] )
       }]
-      
+            
       keyList.push('functions')
     }
       
