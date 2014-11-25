156ae3ae161d72cc61138fb34d12505cdcdcbcfe
charlieroberts <charlie@charlie-roberts.com>
2014-11-25 14:33:43 +0800

    fixed bug in Seq targeting

--- js/index.js
+++ js/index.js
@@ -30271,7 +30271,7 @@ module.exports = function( Gibber ) {
     if( typeof arguments[0]  === 'object' && ! Array.isArray( arguments[0] ) ) {
       var arg = arguments[0],
           durationsType = typeof arg.durations,
-          targetsType = typeof arg.target ,
+          targetsType = typeof arg.target,
           priority = arg.priority,
           hasScale
       
@@ -30284,17 +30284,15 @@ module.exports = function( Gibber ) {
         obj.durations = arg.durations
       }else if( durationsType !== 'undefined') {
         obj.durations = [ arg.durations ]
-      }else{
-        
-      }
-      
+      }else{ }
+            
       obj.keysAndValues = {}
       obj.seqs = []
       obj.autofire = []
       
       for( var key in arg ) {
         if( doNotSequence.indexOf( key ) === -1 ) {
-          var valueType = $.type( arg[ key ] )
+          var isArray = Array.isArray( arg[key] )// $.type( arg[ key ] )
           
           var _seq = {
             key: key,
@@ -30302,9 +30300,9 @@ module.exports = function( Gibber ) {
             durations:obj.durations
           }
           
-          if( valueType === 'array' || typeof arg.length === 'number' ) {
+          if( isArray ) {
             _seq.values = arg[ key ]
-          }else if( valueType !== 'undefined' ) {
+          }else if( typeof arg[ key ] !== 'undefined' ) {
             _seq.values = [ arg[ key ] ]
           }
                     
@@ -30952,7 +30950,7 @@ var teoria = require('../../external/teoria.min'),
 
 var Theory = {
   Teoria: teoria,
-  Scale : function(_root, _mode) {
+  Scale : function( _root, _mode) {
   	var that = {
   		root: typeof _root === "string" ? teoria.note(_root) : _root,
   		notes: [],
@@ -31113,7 +31111,7 @@ var Theory = {
 //       set: function( val ) { mode = val; this.create(); }  
 //     });
     
-    //that.create();
+    that.create();
       
     return that;
   },
