1df5c500467a53ba2026b402e346834198c608c7
charlieroberts <charlie@charlie-roberts.com>
2015-05-13 21:57:04 -0700

    OSC additions

--- js/index.js
+++ js/index.js
@@ -34286,13 +34286,15 @@ module.exports = function( Gibber ) {
   var Comm = {
     export: function( target ) {
       target._MIDI_ = this.MIDI
+      target.OSC = this.OSC
     },
     
-    MIDI: require( './midi.js')( Gibber )
+    MIDI: require( './midi.js' )( Gibber ),
+    OSC:  require( './osc.js' )( Gibber )
   }
   return Comm
 }
-},{"./midi.js":"/www/gibber.libraries/node_modules/gibber.lib/node_modules/gibber.communication.lib/scripts/client/gibber/midi.js"}],"/www/gibber.libraries/node_modules/gibber.lib/node_modules/gibber.communication.lib/scripts/client/gibber/midi.js":[function(require,module,exports){
+},{"./midi.js":"/www/gibber.libraries/node_modules/gibber.lib/node_modules/gibber.communication.lib/scripts/client/gibber/midi.js","./osc.js":"/www/gibber.libraries/node_modules/gibber.lib/node_modules/gibber.communication.lib/scripts/client/gibber/osc.js"}],"/www/gibber.libraries/node_modules/gibber.lib/node_modules/gibber.communication.lib/scripts/client/gibber/midi.js":[function(require,module,exports){
 module.exports = function( Gibber ) {
 
 /* CLOCK
@@ -34634,6 +34636,55 @@ var MIDI = {
 }
 
 return MIDI
+
+}
+},{}],"/www/gibber.libraries/node_modules/gibber.lib/node_modules/gibber.communication.lib/scripts/client/gibber/osc.js":[function(require,module,exports){
+module.exports = function( Gibber ) {
+  
+var OSC = Gibber.OSC = {
+  callbacks : {},
+  
+  init : function( port ) {
+    var _port = port || 8080,
+        _socket = OSC.socket = new WebSocket( 'ws://127.0.0.1:' + _port )
+    
+    OSC.socket.onopen = function() { console.log( 'OSC socket opened on port ' + _port + '.' ) }
+    OSC.socket.onmessage = OSC.onmessage;
+  },
+  
+  onmessage : function(msg) {
+    var data
+    try{
+      data = JSON.parse( msg.data )
+    }catch( error ) {
+      console.error( "ERROR on parsing OSC msg", error )
+      return
+    }
+    
+    if( OSC.callbacks[ data.address ] ) {
+      OSC.callbacks[ data.address ]( data.parameters )
+    }else{
+      if( OSC.callbacks[ '*' ] ) {
+        data.parameters.address = data.address
+        OSC.callbacks[ '*' ]( data.parameters )
+      }
+    }
+  },
+  
+  send : function( address, type, params, port, ipAddress ) {
+    var msg = {
+      'address':address,
+      'type': type.split(''),
+      'parameters':params,
+      'port': port,
+      'ipAddress': ipAddress,
+      type:'OSC'
+    }
+    OSC.socket.send( JSON.stringify( msg ) )
+  },
+}
+
+return OSC
 
 }
 },{}],"/www/gibber.libraries/node_modules/gibber.lib/node_modules/gibber.core.lib/scripts/dollar.js":[function(require,module,exports){
