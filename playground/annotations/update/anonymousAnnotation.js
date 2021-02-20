03133a4ab21c520000ffab05f102eacbf69eca04
charlieroberts <charlie@charlie-roberts.com>
2021-02-19 17:35:32 -0800

    enable turning annotations for anonymous functions off

--- playground/annotations/update/anonymousAnnotation.js
+++ playground/annotations/update/anonymousAnnotation.js
@@ -1,39 +1,45 @@
 module.exports = ( patternObject, marker, className, cm ) => {
   patternObject.commentMarker = marker
-  let update = () => {
+  let out
 
-    if( !patternObject.commentMarker ) return
-    let patternValue = '' + patternObject.update.value
+  if( window.Environment.useComments === true ) {
+    let update = () => {
+
+      if( !patternObject.commentMarker ) return
+      let patternValue = '' + patternObject.update.value
 
 
-    if( patternValue.length > 8 ) patternValue = patternValue.slice(0,8) 
+      if( patternValue.length > 8 ) patternValue = patternValue.slice(0,8) 
 
-    let val ='/* ' + patternValue + ' */',
-      pos = patternObject.commentMarker.find(),
-      end = Object.assign( {}, pos.to )
+      let val ='/* ' + patternValue + ' */',
+        pos = patternObject.commentMarker.find(),
+        end = Object.assign( {}, pos.to )
 
-    //pos.from.ch += 1
-    end.ch = pos.from.ch + val.length 
-    //pos.from.ch += 1
+      //pos.from.ch += 1
+      end.ch = pos.from.ch + val.length 
+      //pos.from.ch += 1
 
-    cm.replaceRange( val, pos.from, pos.to )
+      cm.replaceRange( val, pos.from, pos.to )
 
-    if( patternObject.commentMarker ) patternObject.commentMarker.clear()
+      if( patternObject.commentMarker ) patternObject.commentMarker.clear()
 
-    patternObject.commentMarker = cm.markText( pos.from, end, { className, atomic:false })
+      patternObject.commentMarker = cm.markText( pos.from, end, { className, atomic:false })
 
+    }
+
+    patternObject.clear = () => {
+      try{
+        let commentPos = patternObject.commentMarker.find()
+        //commentPos.to.ch -= 1 // XXX wish I didn't have to do this
+        cm.replaceRange( '', commentPos.from, commentPos.to )
+        patternObject.commentMarker.clear()
+        delete patternObject.commentMarker
+      } catch( e ) {} // yes, I just did that XXX 
+    }
+
+    out = update
   }
 
-  patternObject.clear = () => {
-    try{
-      let commentPos = patternObject.commentMarker.find()
-      //commentPos.to.ch -= 1 // XXX wish I didn't have to do this
-      cm.replaceRange( '', commentPos.from, commentPos.to )
-      patternObject.commentMarker.clear()
-      delete patternObject.commentMarker
-    } catch( e ) {} // yes, I just did that XXX 
-  }
-
-  return update
+  return window.Environment.useComments ? out : ()=>{}
 }
 
