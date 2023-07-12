a4932e7bd94610f91ca345c033353023ee0fc3e3
charlieroberts <charlie@charlie-roberts.com>
2023-07-12 11:49:37 -0700

    added decay to soundfont tutorial

--- playground/examples/soundfont.js
+++ playground/examples/soundfont.js
@@ -29,14 +29,22 @@ s = Soundfont('Acoustic Grand Piano').connect( verb, .25 )
 s.chord( rndi(0,14,6) )
 
 // reverb really helps soundfonts sound better. let's try
-// the same sound with a different bankk for comparison.
+// the same sound with a different bank for comparison.
 
-s2 = Soundfont('Acoustic Grand Piano', { bank: 2 }).connect( verb, .25 )
+s2 = Soundfont('Acoustic Grand Piano', { bank: 4 }).connect( verb, .25 )
 s2.chord( rndi(0,14,6) )
 
-// s2 sounds brighter to me, but I like both patches. There
+// s2 sounds more muted to me, I prefer it. There
 // are five banks available (numbered 0-4); 0 is the default
 // bank if none is specified. 
+
+// you can also change the decay of the soundfont, which 
+// defaults to one second... but it's an exponential decay
+// so one second is fairly sharp. 
+
+// decay measured in samples (sorry!)
+s2.decay = 44100 * 8
+s2.chord( rndi(0,14,6) )
 
 // to see a list of all soundfonts, just place single
 // quotes in between the parenthesis following the 
@@ -49,7 +57,9 @@ Clock.bpm = 140
 s4 = Soundfont('String Ensemble 1', { bank:3 }).connect( verb, 1 )
 s4.chord.seq( Rndi(-14,14,6), 2 )
 s4.gain = .5
+s4,decay = 44100 * 10
  
-s5 = Soundfont('Timpani').connect( verb, .1 )
+s5 = Soundfont('Timpani', { bank:3 ).connect( verb, .1 )
+s5.decay = 44100 * 4
 s5.note.tidal( '<[-14 -14*3] [-12 -12*3]>')
 
